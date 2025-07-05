55640796178315e22db5ac56913e1ccc03f30d44
Felipe Navas <felipenavas.itec@gmail.com>
2025-07-05 10:13:49 -0300

    Add props

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,16 @@
-import { createSignal } from 'solid-js'
+import { createSignal, type Component, type JSX } from 'solid-js'
 
-export const Counter = () => {
-  const [counter, setCounter] = createSignal(10)
+interface Props {
+  initValue?: number
+  children?: JSX.Element
+}
+export const Counter: Component<Props> = (props) => {
+  const [counter, setCounter] = createSignal(props.initValue ?? 0)
 
   return (
     <>
-      <h1 class="text-4xl">Counter</h1>
+      {/* <h1 class="text-4xl">Counter</h1> */}
+      {props.children}
       <h3 class="text-2xl">{counter()}</h3>
       <button
         class="bg-blue-500 p-2 mr-2 rounded"
